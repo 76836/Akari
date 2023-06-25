ba1cc814cb7140247991058ffdfaef0930f251c3
76836 <74463275+76836@users.noreply.github.com>
2023-06-24 20:32:15 -0600

    Update speech.js

--- speech.js
+++ speech.js
@@ -1,48 +1,90 @@
-const synth = window.speechSynthesis;
-
-const inputForm = document.querySelector("form");
-const inputTxt = document.querySelector(".txt");
-const voiceSelect = document.querySelector("select");
-
-const pitch = document.querySelector("#pitch");
-const pitchValue = document.querySelector(".pitch-value");
-const rate = document.querySelector("#rate");
-const rateValue = document.querySelector(".rate-value");
-
+/*
+ * Check for browser support
+ */
+var supportMsg = document.getElementById('test');
 var responsetxt = document.querySelector('.responsetxt');
 
-let voices = [];
+if ('speechSynthesis' in window) {
+	supportMsg.innerHTML = 'Your browser <strong>supports</strong> speech synthesis.';
+} else {
+	supportMsg.innerHTML = 'Sorry your browser <strong>does not support</strong> speech synthesis.<br>Try this in <a href="https://www.google.co.uk/intl/en/chrome/browser/canary.html">Chrome Canary</a>.';
+	supportMsg.classList.add('not-supported');
+}
 
-   function speak(WORDS) {
-     if (synth.speaking) {
-       console.error("speechSynthesis.speaking");
-       return;
-     }
-   
-     if (WORDS !== "") {
-       const utterThis = new SpeechSynthesisUtterance(WORDS);
-   
-       utterThis.onend = function (event) {
-         console.log("SpeechSynthesisUtterance.onend");
-       };
-   
-       utterThis.onerror = function (event) {
-         console.error("SpeechSynthesisUtterance.onerror");
-       };
-   
-       const selectedOption = "Google US English (en-US)"
-   
-       for (let i = 0; i < voices.length; i++) {
-         if (voices[i].name === selectedOption) {
-           utterThis.voice = voices[i];
-           break;
-         }
-       }
-       utterThis.pitch = 1;
-       utterThis.rate = 1;
-       responsetxt.textContent = WORDS;
-       synth.speak(utterThis);
-       
-     }
-   }
 
+// Get the voice select element.
+var voiceSelect = document.getElementById('voice');
+
+// Get the attribute controls.
+var volumeInput = document.getElementById('volume');
+var rateInput = document.getElementById('rate');
+var pitchInput = document.getElementById('pitch');
+
+
+// Fetch the list of voices and populate the voice options.
+function loadVoices() {
+  // Fetch the available voices.
+	var voices = speechSynthesis.getVoices();
+  
+  // Loop through each of the voices.
+	voices.forEach(function(voice, i) {
+    // Create a new option element.
+		var option = document.createElement('option');
+    
+    // Set the options value and text.
+		option.value = voice.name;
+		option.innerHTML = voice.name;
+		  
+    // Add the option to the voice selector.
+		voiceSelect.appendChild(option);
+	});
+}
+
+// Execute loadVoices.
+loadVoices();
+
+// Chrome loads voices asynchronously.
+window.speechSynthesis.onvoiceschanged = function(e) {
+  loadVoices();
+};
+
+
+// Create a new utterance for the specified text and add it to
+// the queue.
+function speak(text) {
+  // Create a new instance of SpeechSynthesisUtterance.
+	var overflow = new SpeechSynthesisUtterance();
+  
+  // Set the text.
+	overflow.text = text;
+  
+  // Set the attributes.
+  overflow.volume = parseFloat(volumeInput.value);
+  overflow.pitch = 1;
+  overflow.rate = 1;
+  overflow.lang = 'en-US';
+
+  // If a voice has been selected, find the voice and set the
+  // utterance instance's voice attribute.
+	if (voiceSelect.value) {
+		overflow.voice = speechSynthesis.getVoices().filter(function(voice) { return voice.name == voiceSelect.value; })[0];
+	}
+  
+  // Queue this utterance.
+	window.speechSynthesis.speak(overflow);
+  responsetxt.textContent = text;
+  overflow.onend = function (event) {
+    console.log("SpeechSynthesisUtterance.onend");
+  };
+
+  overflow.onerror = function (event) {
+    console.error("SpeechSynthesisUtterance.onerror");
+  };
+
+     
+}
+
+
+/*
+ * Mostly copied and pasted from https://codepen.io/matt-west/pen/DpmMgE 
+ */
