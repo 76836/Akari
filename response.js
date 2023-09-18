dd6727b0af11f6ee74d897a3f4b81192ac1b7475
76836 <74463275+76836@users.noreply.github.com>
2023-09-17 19:47:53 -0600

    Update response.js

--- response.js
+++ response.js
@@ -1,5 +1,10 @@
 async function respond(outcome) {
 
+        var incoming = '<p class="command">'+outcome+'</p><br><br><br>' ;
+            var messages = document.getElementById('messages');
+            messages.insertAdjacentHTML('beforebegin', incoming);
+    
+
     outcome = outcome.toLowerCase();
     outcome = outcome.replace('akari', "");
     outcome = outcome.replace(' enter command.', "");
@@ -7,7 +12,7 @@ async function respond(outcome) {
 
     if (outcome.includes("exit ") && outcome.length < 12 == true) {
         console.log('keyword "exit" found');
-        speak('goodbye');
+        say('goodbye');
         await sleep(1000);
         window.close()
 
@@ -18,7 +23,7 @@ async function respond(outcome) {
     if (outcome.includes("time") && outcome.includes("what") == true) {
         console.log('keywords "time" & "what" found');
         var d = new Date();
-        speak('The time is... ' + d.toLocaleTimeString())
+        say('The time is... ' + d.toLocaleTimeString())
 
         return;
     };
@@ -29,15 +34,15 @@ async function respond(outcome) {
         var location = outcome.replace('directions', "");
         location = location.replace(' to ', " ");
         location = location.replace('get ', " ");
-        speak('getting directions to ' + location);
-        //window.location.href='https://www.google.com/maps/search/'+outcome ;
+        say('getting directions to ' + location);
+        window.location.href='https://www.google.com/maps/search/'+outcome ;
         return;
     };
 
 
     if (outcome.includes("help") && outcome.length < 15 == true) {
         console.log('command "help" entered');
-        speak('here is a list of commands to try: open app redirects you to appname.com. play youtube video.  whats the time. get directions to location. exit. help.  or, search.');
+        say('here is a list of commands to try: open app redirects you to appname.com. play youtube video.  whats the time. get directions to location. exit. help.  or, search.');
         return;
     };
 
@@ -47,8 +52,8 @@ async function respond(outcome) {
         var appname = outcome.replace('open', "");
         appname = appname.replace(/\s+/g, '')
         appname = appname.replace('.', "");
-        if (appname.includes("settings")) { speak('opening Akari settings'); window.location.href = './settings'; return; };
-        speak('redirecting you to ' + appname + ".com");
+        if (appname.includes("settings")) { say('opening Akari settings'); window.location.href = './settings'; return; };
+        say('redirecting you to ' + appname + ".com");
         await sleep(300);
         window.location.href = 'https://' + appname + '.com';
         return;
@@ -62,7 +67,7 @@ async function respond(outcome) {
         video = video.replace('play', "");
         video = video.replace(' on ', " ");
         video = video.replace(' for ', " ");
-        speak('looking for ' + video + " on YouTube");
+        say('looking for ' + video + " on YouTube");
         window.location.href = 'https://youtube.com/search?q=' + video;
         return;
     };
@@ -70,7 +75,7 @@ async function respond(outcome) {
 
     if (outcome.includes("destruct") && outcome.includes("self") && outcome.length < 30 == true) {
         console.log('the self destruct program has been launched');
-        if (outcome.includes("1")) { speak('self destruct launched, goodbye.'); } else { speak('passcode incorrect'); return; };
+        if (outcome.includes("1")) { say('self destruct launched, goodbye.'); } else { say('passcode incorrect'); return; };
         //The pasword is actually 1234, but i'm not gonna add the code to check for the full thing.
         document.getElementById('html').innerHTML = '';
         console.clear()
@@ -80,14 +85,14 @@ async function respond(outcome) {
 
     if (outcome.includes("what") && outcome.includes("up") && outcome.length < 18 == true) {
         console.log('kewords "what" & "up" found');
-        speak('version 1.6... now you can say my name at any time to talk to me');
+        say('version 1.6... now you can say my name at any time to talk to me');
         return;
     };
 
 
     if (outcome.includes("hello there") && outcome.length < 19 == true) {
         console.log(' I will deal with this Jedi slime myself. \n   Your move.');
-        speak('General Kenobi. You are a bold one.');
+        say('General Kenobi. You are a bold one.');
 
         return;
     };
@@ -95,9 +100,9 @@ async function respond(outcome) {
     if (outcome.includes("hello") || outcome.includes("hi") && outcome.length < 10 == true) {
         console.log('keword "hello" or "hi" found.');
         if (outcome.includes("hi")) {
-            speak('Hello there.');
+            say('Hello there.');
         }else{
-            speak('Hi!');
+            say('Hi!');
         }
         
 
@@ -108,13 +113,24 @@ async function respond(outcome) {
         console.log('keyword "search" found.');
         var query = outcome.replace('search', "");
         query = query.replace(' for ', " ");
-        speak("searching for " + query);
+        say("searching for " + query);
         await sleep(300);
         window.location.href='https://bing.com/search?q='+query;
         return;
     };
 
+    if (outcome.includes("what is ")) {
+        const mathExpression = outcome.replace("what is ", "");
+        try {
+            const result = eval(mathExpression);
+            say(result);
+        } catch (error) {
+            say('Sorry, I was unable to calculate the given expression.')
+        }
+        return;
+    };
+
     console.log('no keywords found, too bad.');
-    speak("Sorry, I don't understand that command.");
+    say("Sorry, I don't understand that command.");
 
 };
