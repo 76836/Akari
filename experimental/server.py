baaeb794bf163511097b7f82a7bb7e54025f7608
76836 <74463275+76836@users.noreply.github.com>
2023-11-07 21:22:23 -0700

    Update server.py

--- experimental/server.py
+++ experimental/server.py
@@ -1,18 +1,14 @@
 from gpt4all import GPT4All
 import asyncio
 import websockets
+import datetime
+
+print('Akari AI Server v1.1\n')
 
 print('[ok] Preparing AI...')
 
 model = GPT4All("gpt4all-falcon-q4_0.bin", allow_download=False)
-system_template = '''Write detailed quick answers for any question. (Akari AI Server v1.0)'''
 prompt_template = 'USER: {0}\nASSISTANT: '
-def stop_on_token_callback(token_id, token_string):
-    # one sentence is enough:
-    if '\n' in token_string:
-        return False
-    else:
-        return True
 
 
 print('[ok] Server running.')
@@ -24,13 +20,16 @@ async def handle_websocket(websocket, path):
             uprompt = message
             print(f"[in]Received message: {uprompt}")
             if (message == "test"):
-                print('[ok] Testing...')
-                response_message = f"Akari AI v1.0 connected"
+                print('\n[ok]Testing...\n')
+                response_message = f"Akari AI v1.1 connected"
             else:
                 print('[ok] Generating response...')
                 prompts = [uprompt]
+                current_time = datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")
+                system_template = '''You are Akari Crimson AI, you have a kind, joking personality. Write detailed quick answers for any question. Give one answer at a time.
+(Akari AI Server v1.1, system time:'''+current_time+''')'''
                 first_input = system_template + prompt_template.format(prompts[0])
-                response_message = model.generate(first_input, temp=1, callback=stop_on_token_callback)
+                response_message = model.generate(first_input, max_tokens=512, temp=0.7, top_k=40, top_p=0.4, repeat_penalty=1.99, repeat_last_n=512, n_batch=8, n_predict=None, streaming=False,)
             await websocket.send(response_message)
             print(f"[out]Sent message: {response_message}")
 
