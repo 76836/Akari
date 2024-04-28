6215e896a532d9d6cacdb0dd0a0cf9a7a4bfc155
76836 <74463275+76836@users.noreply.github.com>
2024-04-27 18:39:29 -0600

    Update avatar-x.js

--- characters/akari/avatar-x.js
+++ characters/akari/avatar-x.js
@@ -1,5 +1,5 @@
 console.log('Preparing to load Akari Vision...')
-fetch('https://76836.github.io/characters/akari/vision')
+fetch('https://76836.github.io/Akari/characters/akari/vision')
   .then(response => response.text())
   .then(htmlContent => {
     const targetDiv = document.getElementById('avatar');
