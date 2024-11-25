56a569eb7cc34c398a78ee40b44aab72a99a515b
76836 <74463275+76836@users.noreply.github.com>
2024-11-24 19:05:49 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -35,10 +35,11 @@ self.addEventListener('fetch', (event) => {
     try {
       const url = new URL(event.request.url);
 
-      // Apply custom headers only for non Akari files
+      // Apply custom headers only for non Akari settings files
       // I hope this is the last patch I need to make
       // echo above statement
-      if (url.origin === self.location.origin && (url.pathname.startsWith('/Akari') == false)) {
+      // echo above statement
+      if (url.origin === self.location.origin && (url.pathname.startsWith('/Akari/settings/') == false)) {
         const response = await fetch(event.request);
 
         // Create a new Headers object and add the custom headers
